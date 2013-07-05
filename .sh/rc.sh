5ac7fab4857d81257ef6e8a676590cf12372d363
Yves Le Douaron <yves.ledouaron@free.fr>
2013-07-06 01:43:03 +0200

    install improvements

--- .sh/rc.sh
+++ .sh/rc.sh
@@ -4,14 +4,18 @@
 # keychain
 if [ -x  /usr/bin/keychain ] ; then
   #local OLD_DIR=$(pwd)
-  #cd ~/.ssh/ ; 
+  #cd ~/.ssh/ ;
 	local CHAIN="" ;
-  for TMP in `ls -f ~/.ssh/*|grep -E  '(r|d)sa' |grep -v -E "*pub"` ; do
-		if [ -f $TMP -a -f $TMP.pub ] ; then
-			CHAIN="${CHAIN} $TMP" ;
-			keychain -q  $TMP
-		fi
-	done
+  if [[ -d ~/.ssh ]];
+  then
+    for TMP in $(ls -A ~/.ssh/ &> /dev/null |grep -E  '(r|d)sa' |grep -v -E "*pub") ;
+    do
+      if [ -f $TMP -a -f $TMP.pub ] ; then
+        CHAIN="${CHAIN} $TMP" ;
+        keychain -q  $TMP
+      fi
+    done
+  fi
   #cd $OLD_DIR ;
 	#cd -  2>&1 > /dev/null
 	[[ -f ~/.keychain/${HOST}-sh ]] && . ~/.keychain/${HOST}-sh
