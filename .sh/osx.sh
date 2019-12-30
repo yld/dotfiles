3a6913734ca7286397bcb9d3fb2177f358e1a482
Yves Le Douaron <yves@finalcad.com>
2019-12-30 17:51:37 +0100

    Shellcheck typos

--- .sh/osx.sh
+++ .sh/osx.sh
@@ -1,11 +1,11 @@
-if [[ $OS = "OSX" ]] ; then
+if [ "$OS" = "OSX" ] ; then
   # brew GNU coreutils
-  if [[ -d /usr/local/opt/coreutils/libexec/gnubin ]] ; then
+  if [ -d /usr/local/opt/coreutils/libexec/gnubin ] ; then
     PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
     MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
   fi
   # brew postgresql 9.6
-  if [[ -d /usr/local/opt/postgresql@9.6/bin ]] ; then
+  if [ -d /usr/local/opt/postgresql@9.6/bin ] ; then
     PATH="/usr/local/opt/postgresql@9.6/bin:$PATH"
     #LDFLAGS:  -L/usr/local/opt/postgresql@9.6/lib
     #CPPFLAGS: -I/usr/local/opt/postgresql@9.6/include
@@ -14,7 +14,7 @@ if [[ $OS = "OSX" ]] ; then
     #Or, if you don't want/need a background service you can just run:
     #  pg_ctl -D /usr/local/var/postgresql@9.6 start
   fi
-  if [[ -d /usr/local/opt/gpg-agent/bin ]] ; then
+  if [ -d /usr/local/opt/gpg-agent/bin ] ; then
     PATH="/usr/local/opt/gpg-agent/bin:$PATH"
   fi
 fi
